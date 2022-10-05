031f0e82b0ea451ae76fa21d0472cb1953bd266b
MonishacloudS <monishas7122001@gmail.com>
2022-10-06 00:10:03 +0530

    added token

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,6 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 
 import { genPassword, createUser, getUserByName } from "../helper.js"
 
@@ -10,19 +12,51 @@ const router = express.Router();
 //Post method => isnert data into db
 
 router.post("/signup", async (request, response) => {
-    const { username, password} = request.body;
+    const { username, password } = request.body;
     console.log(username, password)   //totally optional
     const isUserExist = await getUserByName(username)
     console.log(isUserExist);
-    
-    if (isUserExist){
+
+    if (isUserExist) {
         response.status(400).send({ message: "Username already taken" });
         return;
     }
-    
+    if (!/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@!#%$]).{8,}$/g.test(password)) {
+        response.status(400).send({ message: "Password pattern does not match" })
+        return;
+    }
+
+
     const hashedPassword = await genPassword(password);
     const result = await createUser(username, hashedPassword)
     response.send(result);
+});
+
+
+//Login 
+
+router.post("/login", async (request, response) => {
+    const { username, password } = request.body;
+    console.log(username, password);
+    //db.movies.insertMany(movies) 
+    const userFromDB = await getUserByName(username)
+    console.log(userFromDB);
+    //username already exist
+    if (!userFromDB) {
+        response.status(400).send({ message: "Invalid credentials" });
+        return;
+    }
+    const storedPassword = userFromDB.password;
+
+    const isPasswordMatch = await bcrypt.compare(password, storedPassword)
+    if (!isPasswordMatch) {
+        response.status(400).send({ message: "Invalid credentials" });
+        return;
+    }
+    //issues token
+    const token = jwt.sign({ id: userFromDB, _id }, process.env.SECRET_KEY);
+    response.send({ message: "successfull login", token: token});
+    // response.send(isPasswordMatch);
 });
 
 
